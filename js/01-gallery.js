d69df2b4a9c142d43a4ee7111c01ac117414d2c9
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-08-28 16:03:09 +0300

    task one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,9 +5,8 @@ import { galleryItems } from './gallery-items.js';
 
 const imgContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-
 imgContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-
+imgContainer.addEventListener('click', onImgClick);
 
 function createGalleryItemsMarkup(items) {
   return items.map(({ preview, original, description }) => {
@@ -18,4 +17,42 @@ function createGalleryItemsMarkup(items) {
 </div>`;
     })
     .join('');
+}
+
+
+function onImgClick(event) {
+  
+  event.preventDefault();
+
+  const isItemImage = event.target.classList.contains('gallery__image');
+
+  console.log(isItemImage);
+  
+  if (!isItemImage) return;
+
+  const currentImgUrl = event.target.dataset.source;
+
+  console.log(currentImgUrl);
+
+  const instance = basicLightbox.create(`<img src="${currentImgUrl}" width="1280" height="auto"/>`,
+    {
+      onShow: (instance) => {
+        document.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+
+    if (!isEscKey) return;
+    
+    instance.close();
+  }
 }

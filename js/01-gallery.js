c93a4ca74d70b3a8872a2cc7562dbd38ad627865
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-08-30 09:15:59 +0300

    homework 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,54 +5,60 @@ import { galleryItems } from './gallery-items.js';
 
 const imgContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-imgContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+
 imgContainer.addEventListener('click', onImgClick);
 
 function createGalleryItemsMarkup(items) {
   return items.map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
+      return `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
     </a>
-</div>`;
-    })
-    .join('');
+</li>`;
+    }).join('');
 }
 
+imgContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
 function onImgClick(event) {
-  
   event.preventDefault();
 
   const isItemImage = event.target.classList.contains('gallery__image');
 
-  console.log(isItemImage);
+  // console.log(isItemImage);
   
-  if (!isItemImage) return;
+  if (!isItemImage) {
+    return;
+  };
+  
+  const currentImg = event.target.dataset.source;
 
-  const currentImgUrl = event.target.dataset.source;
+  // console.log(event.target.dataset.source);
 
-  console.log(currentImgUrl);
-
-  const instance = basicLightbox.create(`<img src="${currentImgUrl}" width="1280" height="auto"/>`,
+  const instance = basicLightbox.create(`<img src="${currentImg}" width="1280"/>`,
     {
-      onShow: (instance) => {
+      onShow: () => {
         document.addEventListener('keydown', onEscKeyPress);
       },
-      onClose: (instance) => {
+
+      onClose: () => {
         document.removeEventListener('keydown', onEscKeyPress);
       },
     }
   );
 
-  instance.show();
+    instance.show();
 
   function onEscKeyPress(event) {
+
     const ESC_KEY_CODE = 'Escape';
     const isEscKey = event.code === ESC_KEY_CODE;
 
-    if (!isEscKey) return;
-    
+    if (!isEscKey) {
+      return;
+    };
+
     instance.close();
   }
 }
